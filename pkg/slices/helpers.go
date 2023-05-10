3588c33085c681328a75ad35f57ccf87bffd94ce
tigransct <tigran@sceyt.com>
2023-05-10 19:14:49 +0400

    remove unnecessary function

--- pkg/slices/helpers.go
+++ pkg/slices/helpers.go
@@ -27,27 +27,6 @@ func ContainsAll[T comparable](s1 []T, s2 ...T) bool {
 	return beforeLength == afterLength
 }
 
-// ContainsAllByPointer checks if contains all for pointer tyeps
-func ContainsAllByPointer[T comparable](s1 []*T, s2 ...*T) bool {
-	vm := make(map[T]struct{})
-	for i := 0; i < len(s1); i++ {
-		if s1[i] == nil {
-			continue
-		}
-		vm[*s1[i]] = struct{}{}
-	}
-	beforeLength := len(vm)
-	for i := 0; i < len(s2); i++ {
-		if s2[i] == nil {
-			continue
-		}
-		vm[*s2[i]] = struct{}{}
-	}
-	afterLength := len(vm)
-
-	return beforeLength == afterLength
-}
-
 func RemoveDuplicates[T comparable](s []T) []T {
 	vm := make(map[T]struct{})
 	for i := 0; i < len(s); i++ {
