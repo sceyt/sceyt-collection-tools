d2160bdd9fc3eb6f90a95847aff49b821d33fb9a
tigransct <tigran@sceyt.com>
2023-05-10 15:17:43 +0400

    contains all by pointer, consider null elements

--- pkg/slices/helpers.go
+++ pkg/slices/helpers.go
@@ -31,10 +31,16 @@ func ContainsAll[T comparable](s1 []T, s2 ...T) bool {
 func ContainsAllByPointer[T comparable](s1 []*T, s2 ...*T) bool {
 	vm := make(map[T]struct{})
 	for i := 0; i < len(s1); i++ {
+		if s1[i] == nil {
+			continue
+		}
 		vm[*s1[i]] = struct{}{}
 	}
 	beforeLength := len(vm)
 	for i := 0; i < len(s2); i++ {
+		if s2[i] == nil {
+			continue
+		}
 		vm[*s2[i]] = struct{}{}
 	}
 	afterLength := len(vm)
